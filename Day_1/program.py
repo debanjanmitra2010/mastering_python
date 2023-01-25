fab146396a24d7f17fb5e8bcb980dfa6d5cb5630
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-01-25 15:56:55 -0500

    Day 1 activities

---
+++ Day_1/program.py
@@ -0,0 +1,11 @@
+# Showing usage of print,input and str function 
+
+print("Hello, World!!")
+# This add a new line
+print("Hi, My Name is Debanjan \n" 
+      "I'm learning Python" )
+# This call input function inside print function
+print("Hello! " + input("What is your name: "))
+
+#printing length of you name using str function 
+print("The number of alphabets in your name is " + str(len( input("What is your name ?"))))
