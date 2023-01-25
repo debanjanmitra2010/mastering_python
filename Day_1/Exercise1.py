3e7ebb2b0975d180f4edd9a08cd21615ad70ff75
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-01-25 17:01:42 -0500

    Day 1

---
+++ Day_1/Exercise1.py
@@ -0,0 +1,8 @@
+a = input("a: ")
+b = input("b: ")
+c=""
+c=a
+a=b
+b=c
+print("a:",a)
+print("b:",b)
---
+++ Day_1/variables.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+length = len(name)
+print("The length of your name is " + str(length))
+
