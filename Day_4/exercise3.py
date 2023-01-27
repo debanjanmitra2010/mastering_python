eef0c8c67e5a77628e0e986419b914609eb55be0
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-01-27 15:36:03 -0500

    Added new codes in Day 4

--- Day_4/exercise3.py
+++ Day_4/exercise3.py
@@ -6,6 +6,8 @@ print("*****************************************")
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
+
+#Nested List 
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
@@ -14,3 +16,5 @@ horizontal = int(position[0])
 vertical = int(position[1])
 
 map[vertical - 1][horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
--- Day_4/nested_list.py
+++ Day_4/nested_list.py
@@ -7,5 +7,10 @@ fruits=['apples','oranges','bananas','mangoes','grapes','strawberry']
 veggies = ['Kale', 'Cabbage', 'Celery', 'Asparagus', 'Lettuce']
 
 combo_list =[fruits,veggies]
+print(fruits[-3])
 
 print(combo_list)
+print(combo_list[0])
+print(combo_list[1])
+print(combo_list[1][2])
+print(combo_list[1][3])
