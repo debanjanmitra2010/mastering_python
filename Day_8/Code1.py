8634f15ae5476e25eb0f1fe9ac7804e6f4e9fea2
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-01-30 14:54:15 -0500

    new code added in Day 8

---
+++ Day_8/Code1.py
@@ -0,0 +1,9 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with(location="Toronto", name="Debanjan")
