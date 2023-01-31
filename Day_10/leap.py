47d305fccab1c02fafd41c04641a38e9b2db1a16
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-01-31 17:35:27 -0500

    Added new codes in Day 10

--- Day_10/leap.py
+++ Day_10/leap.py
@@ -14,7 +14,6 @@ def days_in_month():
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
   
   
-#🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
