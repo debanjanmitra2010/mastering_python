f918d93918eb2d59256d4f8162d4e9fb0f1a613b
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-02-01 09:47:48 -0500

    Blackjack project in Day 11

--- Day_10/Calculator/main.py
+++ Day_10/Calculator/main.py
@@ -15,14 +15,12 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-
 operations = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide
 }
-
 
 def calculator():
     print(logo)
@@ -44,6 +42,5 @@ def calculator():
         else:
             should_continue = False
             calculator()
-
 
 calculator()
---
+++ Day_11/Readme.md
@@ -0,0 +1,1 @@
+![BackJack Flowchart](blackjack.gif)
