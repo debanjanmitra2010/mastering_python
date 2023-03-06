ea07876c71438902aa3b84b3d4607a9ab8ce46d8
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-03-06 11:02:55 -0500

    new code added in Day 20

--- Day_20/main.py
+++ Day_20/main.py
@@ -1,29 +1,28 @@
-from turtle import Screen
-from snake import Snake
-import time
+from turtle import Screen, Turtle
 
 # Screen
 screen = Screen()
-screen.setup(600, 600)
+screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
-screen.tracer(0)
 
-# Snake object
-snake = Snake()
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-# Listen to keystrokes
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
-game_is_on = True
-
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+# Without Loop Code
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+#
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20,0)
+#
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40,0)
 
 screen.exitonclick()
