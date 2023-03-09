b9fd702c88dddbb2f7633bb846567fe30d1fa03a
Debanjan Mitra <debanjanmitra2010@gmail.com>
2023-03-09 10:38:24 -0500

    new code added in Day 20

--- Day_20/main.py
+++ Day_20/main.py
@@ -1,17 +1,30 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
-# Screen
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('Snake Game')
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
 
 # Without Loop Code
 # segment_1 = Turtle("square")
@@ -24,5 +37,3 @@ for position in starting_positions:
 # segment_3 = Turtle("square")
 # segment_3.color("white")
 # segment_3.goto(-40,0)
-
-screen.exitonclick()
